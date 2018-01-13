c65baca2f511d8802ce1fdcd43a77551ae4cf5db
potmadu <potmadu@gmail.com>
2018-01-13 22:06:44 +0700

    Add datasets and automation

---
+++ Supervised Learning/Supervised Learning/automation.R
@@ -0,0 +1,257 @@
+#################################
+# Fungsi dan Prosedur
+#################################
+
+baca = function(nama_file) {
+
+    output = read.csv(paste("Dataset/",nama_file,sep = ""), stringsAsFactors = FALSE);
+
+    return(output);
+
+}
+
+partisi = function(input_data,response_name) {
+
+    #70/30 partition
+    #column response as y
+
+    colnames(input_data)[colnames(input_data) == response_name] = "respon";
+    partition_data = createDataPartition(input_data$respon, p = 0.7, list = FALSE, times = 1);
+
+    dataTrain = input_data[partition_data,];
+    dataTrain$tipe = "training";
+    dataTest = input_data[-partition_data,];
+    dataTest$tipe = "testing";
+
+    output_data = rbind(dataTrain, dataTest);
+
+    return(output_data);
+
+}
+
+hitungSVM = function(input_data,target_respon,nu_value) {
+
+    library(e1071);
+
+    train_data = subset(input_data, tipe == "training");
+    train_data$tipe = NULL;
+    test_data = subset(input_data, tipe == "testing");
+    test_data$tipe = NULL;
+
+    df = subset(train_data, respon == target_respon);
+    x = subset(df, select = -respon);
+    y = df$respon;
+
+    model = svm(x, y, type = 'one-classification',nu=nu_value);
+
+    pred_trains = predict(model, x);
+    pred_test = predict(model, subset(test_data, select = -respon));
+
+    pred_data = test_data;
+    pred_data$pred = pred_test;
+    pred_data$flag = 0;
+    pred_data$flag[pred_data$respon == target_respon & pred_data$pred == TRUE] = 1;
+    pred_data$flag[pred_data$respon != target_respon & pred_data$pred == FALSE] = 1;
+
+    print(model);
+
+    pred_train = df;
+    pred_train$pred = pred_trains;
+    pred_train$flag = 1;
+
+    print(paste("Training Accuracy : ", nrow(subset(pred_train, pred_train$pred == TRUE)) / nrow(pred_train)));
+    print(paste("Testing Accuracy : ", nrow(subset(pred_data, flag == 1)) / nrow(pred_data)));
+
+    pred_train$tipe = "TRAIN";
+    pred_data$tipe = "TEST";
+
+    train_test_data = rbind(pred_train, pred_data);
+
+    return(train_test_data);
+
+}
+
+hitungSVM_multi = function(input_data, target_respon) {
+
+    library(e1071);
+
+    train_data = subset(input_data, tipe == "training");
+    train_data$tipe = NULL;
+    test_data = subset(input_data, tipe == "testing");
+    test_data$tipe = NULL;
+
+    df = subset(train_data);
+    x = subset(df, select = -respon);
+    y = as.factor(df$respon);
+
+    model = svm(x, y, probability = TRUE);
+    pred_train = predict(model, x, decision.values = TRUE, probability = TRUE);
+    pred_test = predict(model, subset(test_data, select = -respon),decision.values = TRUE, probability = TRUE);
+
+    #normalization and find specific prediction value 
+    pred_train_value = as.data.frame(attr(pred_train, "probabilities"));
+    pred_test_value = as.data.frame(attr(pred_test, "probabilities"));
+
+    pred_train_value$pred = -1;
+    for (i in 1:nrow(pred_train_value)) {
+
+        max_prob = pred_train_value[i,1];
+        max_pos = 1;
+
+        for (j in 2:5) {
+            current_prob = pred_train_value[i, j];
+            current_pos = j;
+
+            if (current_prob>max_prob) {
+                max_prob = current_prob;
+                max_pos = current_pos;
+            }
+        }
+
+        pred_train_value$pred[i] = max_pos;
+    }
+
+    pred_test_value$pred = -1;
+    for (i in 1:nrow(pred_test_value)) {
+
+        max_prob = pred_test_value[i, 1];
+        max_pos = 1;
+
+        for (j in 2:5) {
+            current_prob = pred_test_value[i, j];
+            current_pos = j;
+
+            if (current_prob > max_prob) {
+                max_prob = current_prob;
+                max_pos = current_pos;
+            }
+        }
+
+        pred_test_value$pred[i] = max_pos;
+    }
+
+    pred_data = test_data;
+    pred_data$pred = pred_test_value$pred;
+    pred_data$flag = 0;
+    pred_data$flag[pred_data$respon == pred_data$pred] = 1;
+
+    print(model);
+
+    pred_train = df;
+    pred_train$pred = pred_train_value$pred;
+    pred_train$flag = 0;
+    pred_train$flag[pred_train$respon == pred_train$pred] = 1;
+
+    print(paste("Training Accuracy : ", nrow(subset(pred_train, pred_train$pred == TRUE)) / nrow(pred_train)));
+    print(paste("Testing Accuracy : ", nrow(subset(pred_data, flag == 1)) / nrow(pred_data)));
+
+    pred_train$tipe = "TRAIN";
+    pred_data$tipe = "TEST";
+
+    train_test_data = rbind(pred_train, pred_data);
+
+    return(train_test_data);
+
+}
+
+hitungDecisionTree = function(input_data) {
+
+}
+
+hitungRandomForest = function(train_data, test_data) {
+
+    train_data$respon = as.factor(train_data$respon);
+    test_data$respon = as.factor(test_data$respon);
+
+    model = randomForest(respon ~ ., data = train_data);
+    pred = predict(model, newdata = test_data);
+    print(table(pred, test_data$respon));
+    print(confusionMatrix(pred, test_data$respon));
+
+}
+
+hitungNaiveBayes = function(input_data) {
+
+}
+
+hitungFeaturesImportance = function(train_data,test_data) {
+
+    # ensure results are repeatable
+    set.seed(7)
+    # load the library
+    library(mlbench)
+    library(caret)
+    # load the dataset
+    input_data = rbind(train_data, test_data)
+    input_data$respon = as.factor(input_data$respon);
+    # prepare training scheme
+    control <- trainControl(method = "repeatedcv", number = 10, repeats = 3)
+    # train the model
+    model <- train(respon ~ ., data = input_data, method = "lvq", preProcess = "scale", trControl = control)
+    # estimate variable importance
+    importance <- varImp(model, scale = FALSE)
+    # summarize importance
+    print(importance)
+    # plot importance
+    plot(importance)
+
+}
+
+#################################
+# Main Program
+#################################
+
+library(imputeTS);
+library(dplyr);
+
+train_file = "Training Data.csv";
+test_file = "Test Data.csv";
+target_column = "manual_assessment";
+
+train_data = baca(train_file);
+colnames(train_data)[colnames(train_data) == target_column] = "respon";
+
+test_data = baca(test_file);
+colnames(test_data)[colnames(test_data) == target_column] = "respon";
+
+train_data %>%
+filter(is.na(respon) | is.na(manual_origin_source) | is.na(origin_word_entropy)) %>%
+group_by(manual_origin_source,origin_word_entropy,respon) %>%
+summarise(jumlah_data = n());
+
+#Dataset1 = manual_assessment as target
+train_dataset1 = train_data;
+test_dataset1 = test_data;
+
+#Data Preprocessing
+train_dataset1$username = NULL;
+train_dataset1$source.word = NULL;
+train_dataset1$target.word = NULL;
+train_dataset1_assessed = train_dataset1 %>%
+filter(!is.na(respon) & !is.na(manual_origin_source) & !is.na(origin_word_entropy)) %>%
+as.data.frame();
+#check na value
+colnames(train_dataset1_assessed)[colSums(is.na(train_dataset1_assessed)) > 0]
+#normalization by replace NA with 0
+train_dataset1_norm = na.replace(train_dataset1_assessed, 0);
+colnames(test_dataset1_norm)[colSums(is.na(test_dataset1_norm)) > 0]
+
+test_dataset1$username = NULL;
+test_dataset1$source.word = NULL;
+test_dataset1$target.word = NULL;
+test_dataset1_assessed = test_dataset1 %>%
+filter(!is.na(respon) & !is.na(manual_origin_source) & !is.na(origin_word_entropy)) %>%
+as.data.frame();
+#check na value
+colnames(test_dataset1_assessed)[colSums(is.na(test_dataset1_assessed)) > 0]
+#normalization by replace NA with 0
+test_dataset1_norm = na.replace(test_dataset1_assessed, 0);
+colnames(test_dataset1_norm)[colSums(is.na(test_dataset1_norm)) > 0]
+
+######### KALKULASI
+
+hitungFeaturesImportance(train_dataset1_norm,test_dataset1_norm);
+
+modelSVM = hitungSVM(dataset1_part, 5, 0.1);
+modelRandomForest = hitungRandomForest(train_dataset1_norm, test_dataset1_norm);
+
--- Supervised Learning/Supervised Learning/supervised_learning.R
+++ Supervised Learning/Supervised Learning/supervised_learning.R
@@ -4,3 +4,296 @@ manual_check = read.csv("Dataset/Manual quality check TG2.csv", stringsAsFactors
 
 manual_check = manual_check[, 1:19];
 
+####################################
+# SVM
+####################################
+
+library(e1071);
+
+data(iris);
+df = iris;
+
+df = subset(df, Species == 'setosa');
+
+x = subset(df, select = -Species);
+y = df$Species;
+model = svm(x, y, type = 'one-classification',nu=0.10);
+
+print(model);
+summary(model);
+
+pred_train = predict(model, x);
+pred_test = predict(model, subset(iris, select = -Species));
+
+pred_data = iris;
+pred_data$pred = pred_test;
+pred_data$flag = 0;
+pred_data$flag[pred_data$Species == 'setosa' & pred_data$pred == TRUE] = 1;
+nrow(subset(pred_data, flag == 1)) / nrow(pred_data);
+
+pred_train = as.data.frame(pred_train);
+nrow(subset(pred_train, pred_train == TRUE)) / nrow(pred_train);
+
+data(iris);
+attach(iris);
+x = subset(iris, select = -Species);
+y = Species;
+
+model = svm(x, y, probability = TRUE);
+pred_prob = predict(model, x, decision.values = TRUE, probability = TRUE);
+
+####################################
+# 10-folds Cross Validation
+####################################
+
+library(caret);
+
+folds = createFolds(end_dat, k = 10, list = TRUE, returnTrain = FALSE);
+
+####################################
+
+svm_tune = tune(svm, x, y, type = 'one-classification', ranges = list(gamma = seq(0.05, 1, by = 0.05), nu = seq(0.1, 1, by = 0.1)));
+
+svm_tune = tune(svm, x, z, type = 'one-classification', nu = seq(0.1, 1, by = 0.1), gamma = seq(0.05, 1, by = 0.05));
+
+svm_tune = tune(svm, Species ~ ., data = iris, type='one-classification',
+ranges = list(gamma = 2 ^ (-1:1), cost = 2 ^ (2:4))
+)
+
+svm_cross10 = tune.svm(x,y,subset(iris,select = -Species),nu = 0.1,gamma = 0.5,tunecontrol=)
+
+####################################
+# Naive Bayes
+####################################
+
+data(HouseVotes84, package = "mlbench")
+model <- naiveBayes(Class ~ ., data = HouseVotes84)
+pred <- predict(model, HouseVotes84)
+table(pred, HouseVotes84$Class)
+
+library(e1071)
+model <- naiveBayes(class ~ ., data = breast_cancer)
+class(model)
+summary(model)
+print(model)
+preds = predict(model, newdata = breast_cancer);
+
+library(e1071);
+
+train_data = subset(input_data, tipe == "training");
+train_data$tipe = NULL;
+test_data = subset(input_data, tipe == "testing");
+test_data$tipe = NULL;
+
+train_data$respon = as.factor(train_data$respon);
+test_data$respon = as.factor(test_data$respon);
+
+model = naiveBayes(respon ~ ., data = train_data);
+preds = predict(model, newdata=test_data);
+confusionMatrix(preds, test_data$respon);
+
+model = naiveBayes(respon ~ ., data = train_data, laplace = 1);
+preds = predict(model, newdata = test_data);
+conf_matrix = table(preds, test_data$respon);
+conf_matrix;
+
+model = naiveBayes(respon ~ ., data = train_data, laplace = 2);
+preds = predict(model, newdata = test_data);
+conf_matrix = table(preds, test_data$respon);
+conf_matrix;
+
+model = naiveBayes(respon ~ ., data = train_data, laplace = 3);
+preds = predict(model, newdata = test_data);
+conf_matrix = table(preds, test_data$respon);
+conf_matrix;
+
+####################################
+# Random Forest
+####################################
+
+library(randomForest);
+library(MASS);
+
+attach(Boston);
+
+dim(Boston);
+set.seed(101);
+
+train = sample(1:nrow(Boston), 300);
+
+Boston.rf = randomForest(medv ~ ., data = Boston, subset = train);
+
+Boston.rf;
+
+oob.err = double(13);
+test.err = double(13);
+
+for (mtry in 1:13) {
+    rf = randomForest(medv ~ ., data = Boston, subset = train, mtry = mtry, ntree = 400);
+    oob.err[mtry] = rf$mse[400];
+    pred = predict(rf, Boston[-train,]);
+    test.err[mtry] = with(Boston[-train,], mean((medv - pred) ^ 2));
+    cat(mtry," ")
+}
+
+library(randomForest)
+library(mlbench)
+library(caret)
+# Load Dataset
+data(Sonar)
+dataset <- Sonar
+x <- dataset[, 1:60]
+y <- dataset[, 61]
+control <- trainControl(method = "repeatedcv", number = 10, repeats = 3)
+seed <- 7
+metric <- "Accuracy"
+set.seed(seed)
+mtry <- sqrt(ncol(x))
+tunegrid <- expand.grid(.mtry = mtry)
+rf_default <- train(Class ~ ., data = dataset, method = "rf", metric = metric, tuneGrid = tunegrid, trControl = control)
+print(rf_default)
+
+url <- 'https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv'
+wine <- read.table(url)
+
+library(randomForest)
+
+train_data$respon = as.factor(train_data$respon);
+test_data$respon = as.factor(test_data$respon);
+
+model <- randomForest(respon ~ ., data = train_data)
+pred <- predict(model, newdata = test_data)
+table(pred, test_data$respon)
+confusionMatrix(pred, test_data$respon)
+
+####################################
+# Decision Tree
+####################################
+
+library(rpart);
+fit = rpart(Kyphosis ~ Age + Number + Start, data = kyphosis);
+fit2 = rpart(Kyphosis ~ Age + Number + Start, data = kyphosis,parms = list(prior=c(.65,.35),split="information"));
+fit3 = rpart(Kyphosis ~ Age + Number + Start, data = kyphosis, control = rpart.control(cp = 0.05));
+par(mfrow = c(1, 2), xpd = NA);
+plot(fit);
+text(fit, use.n = TRUE);
+plot(fit2);
+text(fit2, use.n = TRUE);
+
+#CART
+
+model = rpart(respon ~ ., data = train_data, method = "class");
+pred = predict(model, test, type = "class");
+confusionMatrix(pred, test_data$respon)
+
+#C4.5
+
+library(RWeka);
+library(caret);
+
+set.seed(1958);
+model = train(respon ~ ., data = train_data, method = "J48");
+pred = predict(model, test_data);
+confusionMatrix(pred, test_data$respon);
+
+#C5.0
+
+library(C50);
+library(printr);
+
+model = C5.0(respon ~ ., data = train_data);
+results = predict(object = model, newdata = test_data, type = "class");
+pred = predict(model, test_data);
+confusionMatrix(pred, test_data$respon);
+
+#Information Index
+
+#Gini index
+
+
+###################################
+
+z.auto = rpart(Mileage ~ Weight, car.test.frame);
+predict(z.auto);
+
+fit = rpart(Kyphosis ~ Age + Number + Start, data = kyphosis);
+predict(fit, type = "prob");
+predict(fit, type = "vector");
+predict(fit, type = "class");
+predict(fit, type = "matrix");
+
+sub = c(sample(1:50, 25), sample(51:100, 25), sample(101:150, 25));
+fit = rpart(Species ~ ., data = iris, subset = sub);
+fit;
+table(predict(fit, iris[-sub,], type = "class"), iris[-sub, "Species"]);
+
+####################################
+# Softmax Classifier
+####################################
+
+library(nnet);
+
+seeds = read.csv("seeds.csv", header = T);
+
+####################################
+# Feature Selections
+####################################
+
+######### FIND CORRELATED FEATURES
+
+# ensure the results are repeatable
+set.seed(7)
+# load the library
+library(mlbench)
+library(caret)
+# load the data
+data(PimaIndiansDiabetes)
+# calculate correlation matrix
+correlationMatrix <- cor(PimaIndiansDiabetes[, 1:8])
+# summarize the correlation matrix
+print(correlationMatrix)
+# find attributes that are highly corrected (ideally >0.75)
+highlyCorrelated <- findCorrelation(correlationMatrix, cutoff = 0.5)
+# print indexes of highly correlated attributes
+print(highlyCorrelated)
+
+######### RANK FEATURES BY IMPORTANCE
+
+# ensure results are repeatable
+set.seed(7)
+# load the library
+library(mlbench)
+library(caret)
+# load the dataset
+data(PimaIndiansDiabetes)
+# prepare training scheme
+control <- trainControl(method = "repeatedcv", number = 10, repeats = 3)
+# train the model
+model <- train(diabetes ~ ., data = PimaIndiansDiabetes, method = "lvq", preProcess = "scale", trControl = control)
+# estimate variable importance
+importance <- varImp(model, scale = FALSE)
+# summarize importance
+print(importance)
+# plot importance
+plot(importance)
+
+######### RECURSIVE FEATURE ELIMINATION
+
+# ensure the results are repeatable
+set.seed(7)
+# load the library
+library(mlbench)
+library(caret)
+# load the data
+data(PimaIndiansDiabetes)
+# define the control using a random forest selection function
+control <- rfeControl(functions = rfFuncs, method = "cv", number = 10)
+# run the RFE algorithm
+results <- rfe(PimaIndiansDiabetes[, 1:8], PimaIndiansDiabetes[, 9], sizes = c(1:8), rfeControl = control)
+# summarize the results
+print(results)
+# list the chosen features
+predictors(results)
+# plot the results
+plot(results, type = c("g", "o"))
+
